505b0b738f5636bf7dfe057d75f956b36fbffc6e
Eugenio Militerno <82472181+BadBro00@users.noreply.github.com>
2024-08-21 20:15:47 +0200

    Create BucketSort.cpp

---
+++ CODICI/ALGORITMI/ORDINAMENTO/PSEUDOCODICI/BucketSort.cpp
@@ -0,0 +1,11 @@
+void BucketSort(arr[])
+  buckets[n]<-new array of buckets
+  for (i<-1 to n)
+    bucketIndex<-n * arr[i];
+    buckets[bucketIndex]<-arr[i]
+  for (i<-1 to n)
+    //Ordina i bucket con un algoritmo
+  index<-0;
+  for (i<-1 to n)
+  for (j<-0 to buckets[i].size
+    arr[index] = buckets[i][j]
